2a50ffb1472f1e69741ee77ab04576ee978ac880
xuhongda <xuhongda7@outlook.com>
2018-07-26 00:17:05 +0800

    java 参数传递问题

--- src/main/java/com/xu/param/ParamTest2.java
+++ src/main/java/com/xu/param/ParamTest2.java
@@ -1,5 +1,7 @@
 package com.xu.param;
 
+import com.xu.pojo.People;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,15 +15,33 @@ public class ParamTest2 {
     public void method1(List list){
         ArrayList ll = (ArrayList) list;
 
-        //浅拷贝
+        //浅拷贝???
         Object clone = ll.clone();
+        System.out.println(clone == list);
         method2(list);
         System.out.println(list);
         System.out.println(clone);
     }
     public List method2(List list){
+
         List list1 = new ArrayList();
         list1.add("yy");
+        list.addAll(list1);
+        return list1;
+    }
+
+    public void method3(List<People> list){
+        ArrayList ll = (ArrayList) list;
+        Object clone = ll.clone();
+        method4(list);
+        System.out.println(ll == list);
+        System.out.println(list);
+        System.out.println(clone);
+    }
+
+    public List method4(List<People> list){
+        List<People> list1 = new ArrayList();
+        list1.add(new People("liu",18));
         list.addAll(list1);
         return list1;
     }
@@ -34,5 +54,9 @@ public class ParamTest2 {
         ParamTest2 paramTest2 = new ParamTest2();
         paramTest2.method1(list);
         System.out.println(list);
+        /**/
+        List<People> peopleList = new ArrayList<>();
+        peopleList.add(new People("xx",18));
+        paramTest2.method3(peopleList);
     }
 }
