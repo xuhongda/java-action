7686c95a5f21e37014b4e726de77e640e350e8a2
xuhongda <xuhongda7@outlook.com>
2018-08-16 23:41:05 +0800

    java 线程 原子性

--- se-practice/src/main/java/com/xu/thread/atom/AtomDemo.java
+++ se-practice/src/main/java/com/xu/thread/atom/AtomDemo.java
@@ -14,32 +14,61 @@ import java.util.concurrent.atomic.AtomicInteger;
  * javase-practice
  */
 public class AtomDemo {
+
     public static void main(String[] args) {
+
+            open1();
+
+            open2();
+    }
+
+    static void open1(){
         RunnableThread thread = new RunnableThread();
         for (int i = 0; i <10 ; i++) {
             new Thread(thread).start();
         }
-
     }
 
-
+    static void open2(){
+        AtomicT thread = new AtomicT();
+        for (int i = 0; i <10 ; i++) {
+            new Thread(thread).start();
+        }
+    }
 }
 
 class RunnableThread implements Runnable{
     private    int num;
-    //protected AtomicInteger atomicInteger = new AtomicInteger();
     @Override
     public  void run() {
         try {
              Thread.sleep(Math.round(Math.random()*100));
             num++;
-            //原子自增
-         // atomicInteger.incrementAndGet();
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
        System.out.println(Thread.currentThread().getName()+"\t"+num);
-      // System.out.println(Thread.currentThread().getName()+"\tatomic\t"+atomicInteger);
+    }
+}
+
+
+class AtomicT implements Runnable{
+
+    protected AtomicInteger atomicInteger = new AtomicInteger();
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(Math.round(Math.random()*100));
+            //原子自增
+            atomicInteger.incrementAndGet();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(Thread.currentThread().getName()+"\tatomic\t"+atomicInteger);
+
     }
 }
