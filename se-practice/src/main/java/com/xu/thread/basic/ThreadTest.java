83f0a5ddb727688137740d02fc9518767d903909
xuhongda <xuhongda7@outlook.com>
2018-11-05 17:09:18 +0800

    获取 jvm info

---
+++ se-practice/src/main/java/com/xu/thread/basic/ThreadTest.java
@@ -0,0 +1,29 @@
+package com.xu.thread.basic;
+
+import org.junit.Test;
+
+/**
+ * @author xuhongda on 2018/11/5
+ * com.xu.thread.basic
+ * javase-practice
+ */
+public class ThreadTest {
+    @Test
+    public void test1() {
+        Runtime runtime = Runtime.getRuntime();
+        //获得处理器核心数
+        int i = runtime.availableProcessors();
+        System.out.println(i);
+
+        //以下是java 虚拟机内存情况
+        long totalMemory = runtime.totalMemory();
+        System.out.println(totalMemory);
+
+        long maxMemory = runtime.maxMemory();
+        System.out.println(maxMemory);
+
+        long freeMemory = runtime.freeMemory();
+        System.out.println(freeMemory);
+
+    }
+}
