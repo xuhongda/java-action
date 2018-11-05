eeb8a57a4de990f7841990d85cd938a006a7dc89
xuhongda <xuhongda7@outlook.com>
2018-11-05 16:03:10 +0800

    transient 关键字 序列化与反序列化

--- se-practice/src/main/java/com/xu/serializable/SerTest.java
+++ se-practice/src/main/java/com/xu/serializable/SerTest.java
@@ -42,6 +42,7 @@ public class SerTest {
      */
     @org.junit.Test
     public void test2() throws IOException, ClassNotFoundException {
+        //序列化
         FileOutputStream fileOutputStream = new FileOutputStream("t.txt");
         Girl girl = new Girl();
         girl.setWeight(48.9);
@@ -50,10 +51,9 @@ public class SerTest {
         ObjectOutputStream outputStream = new ObjectOutputStream(fileOutputStream);
         outputStream.writeObject(girl);
         outputStream.close();
-
+        //反序列化
         File f = new File("t.txt");
         FileInputStream fileInputStream = new FileInputStream(f);
-
         ObjectInputStream inputStream = new ObjectInputStream(fileInputStream);
         Girl g = (Girl) inputStream.readObject();
         System.out.println(g);
