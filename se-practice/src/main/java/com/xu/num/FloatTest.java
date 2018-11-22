cec38445c05c226d0d782d299cc24c1dc3eb5573
xuhongda <xuhongda7@outlook.com>
2018-11-22 16:28:46 +0800

    Float Double 精度问题

---
+++ se-practice/src/main/java/com/xu/num/FloatTest.java
@@ -0,0 +1,51 @@
+package com.xu.num;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+/**
+ * <p>
+ * double 与 float 精度问题
+ * </p>
+ *
+ * @author xuhongda on 2018/11/22
+ * com.xu.num
+ * java-action
+ */
+public class FloatTest {
+    @Test
+    public void test1() {
+        Float f1 = 96515.84F;
+        Float f2 = 568.19F;
+        float f = f1 - f2;
+        System.out.println(f);
+        BigDecimal num1 = new BigDecimal(Float.toString(f1));
+        BigDecimal num2 = new BigDecimal(Float.toString(f2));
+        BigDecimal subtract = num1.subtract(num2);
+        System.out.println(subtract.floatValue());
+
+    }
+
+    @Test
+    public void test2() {
+        Float f1 = 11032.51f;
+        Float f2 = 80800.240f;
+        Float f3 = 8700.00f;
+        Float f4 = 0f;
+        Float f5 = 802400.040f;
+        Float f6 = 0f;
+        float v = f1 * f2 + f3 * f4 + f5 * f6;
+        float ff = v / f2 + f4 + f6;
+        System.out.println(ff);
+
+        BigDecimal b1 = new BigDecimal(Float.toString(f1));
+        BigDecimal b2 = new BigDecimal(Float.toString(f2));
+        BigDecimal b3 = new BigDecimal(Float.toString(f3));
+        BigDecimal b4 = new BigDecimal(Float.toString(f4));
+        BigDecimal add = b1.multiply(b2).add(b3.multiply(b4));
+        BigDecimal divide = add.divide(b2.add(b4), 2, 4);
+        System.out.println(divide.floatValue());
+
+    }
+}
