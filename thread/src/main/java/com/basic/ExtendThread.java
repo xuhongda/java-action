30cbca6d0cbaa5511da8d7ba499501bb75c0d55a
xuhongda <xuhongda7@outlook.com>
2019-04-10 15:38:08 +0800

    内存可见性问题

--- thread/src/main/java/com/basic/ExtendThread.java
+++ thread/src/main/java/com/basic/ExtendThread.java
@@ -1,16 +1,19 @@
 package com.basic;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author xuhongda on 2018/8/16
  * com.basic
  * javase-practice
  */
+@Slf4j
 public class ExtendThread extends Thread {
 
 
     @Override
     public void run(){
         Ticket.func1();
-        System.out.println("is run "+Thread.currentThread().getName());
+        log.info("thread name = {}",Thread.currentThread().getName());
     }
 }
--- thread/src/main/java/com/basic/VolatileTest.java
+++ thread/src/main/java/com/basic/VolatileTest.java
@@ -1,46 +1,60 @@
 package com.basic;
 
+import lombok.Data;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * <p>
- * 内存可见性问题
+ * 内存可见性问题demo,会导致最终值问题
  * </p>
  *
  * @author xuhongda on 2018/8/16
  * com.basic
  * javase-practice
  */
+@Slf4j
 public class VolatileTest {
+
+    private long num = 0L;
+
+    public long increase() {
+        num++;
+        return num;
+    }
+
     public static void main(String[] args) throws InterruptedException {
         MyRunnable thread = new MyRunnable();
-        new Thread(thread).start();
-        // Thread.sleep(10);
-        while (true) {
-            if (thread.isFlag()) {
-                System.out.println("<><><><>");
-                break;
-            }
+        ExecutorService executorService = Executors.newFixedThreadPool(30);
+        for (int i = 0; i < 600; i++) {
+            executorService.submit(thread);
         }
+        executorService.shutdown();
     }
 }
 
+@Slf4j
+@Data
+@ToString
 class MyRunnable implements Runnable {
+
     /**
      * volatile 关键字
      * 注意： volatile 不具备互斥性；不保证原子性
      */
-    private volatile boolean flag = false;
+    VolatileTest volatileTest = new VolatileTest();
 
     @Override
     public void run() {
-        flag = true;
-        System.out.println(flag);
-    }
-
-    public boolean isFlag() {
-        return flag;
-    }
-
-    public void setFlag(boolean flag) {
-        this.flag = flag;
+        long increase = volatileTest.increase();
+        try {
+            Thread.sleep(500L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(increase + "\t" + Thread.currentThread().getName());
     }
 }
