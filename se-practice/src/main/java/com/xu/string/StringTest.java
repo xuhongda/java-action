64070d36e6fae9a4414fb8088124e215823264f8
xuhongda <xuhongda7@outlook.com>
2019-05-13 17:53:08 +0800

    string test

--- se-practice/src/main/java/com/xu/string/StringTest.java
+++ se-practice/src/main/java/com/xu/string/StringTest.java
@@ -1,9 +1,19 @@
 package com.xu.string;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Test;
+import pojo.CommonRequest;
+import pojo.Detail;
+import pojo.DrivingFingerprintIndexMatch;
+import pojo.DrivingFingerprintIndexMatchResponse;
+
+import java.io.IOException;
+import java.util.*;
 
 /**
  * @author xuhongda on 2018/8/31
@@ -13,7 +23,7 @@ import org.junit.Test;
 @Slf4j
 public class StringTest {
 
-
+    private ObjectMapper mapper = new ObjectMapper();
     @Test
     public void test001() {
         String s1 = "hello";
@@ -49,4 +59,56 @@ public class StringTest {
         char b = 'B';
         Assert.assertTrue("a>b", a < b);
     }
+
+
+    @Test
+    public void test6() throws JsonProcessingException {
+        CommonRequest request = new CommonRequest();
+        request.setCmd("xx");
+        Map<String, Object> map = new HashMap<>();
+        map.put("11", "11");
+        map.put("22", "22");
+        request.setParams(map);
+        List<Object[]> list = new ArrayList<>();
+        Integer[] i = {1, 2, 4, 5};
+        list.add(i);
+        request.setList(list);
+        String s = mapper.writeValueAsString(request);
+        System.out.println(s);
+    }
+
+
+    @Test
+    public void test() throws IOException {
+        DrivingFingerprintIndexMatchResponse response = new DrivingFingerprintIndexMatchResponse();
+        response.setCmd("baidu");
+        response.setPageNo(1);
+        response.setResultNote("SUCCESS");
+        response.setTotalRecordNum(1);
+        response.setPageNo(11);
+        response.setPages(77);
+        Detail detail = new Detail();
+        DrivingFingerprintIndexMatch indexMatch = new DrivingFingerprintIndexMatch();
+        indexMatch.setTrackId("11111111111");
+
+        List<DrivingFingerprintIndexMatch> list = Arrays.asList(indexMatch);
+        detail.setDataList(list);
+        response.setDetail(detail);
+
+        String s = mapper.writeValueAsString(response);
+        System.out.println(s);
+
+        DrivingFingerprintIndexMatchResponse response1 = mapper.readValue(s, DrivingFingerprintIndexMatchResponse.class);
+        System.out.println(response1);
+    }
+
+    @Test
+    public void test7() throws IOException {
+        String json = "{\"cmd\":\"getDrivingFingerprintIndexMatch\",\"result\":0,\"resultNote\":\"SUCCESS\",\"totalRecordNum\":1,\"pages\":1,\"pageNo\":1,\"detail\":{\"dataList\":[{\"trackId\":\"000000C1AB10E091F1739560C80D0042\",\"objId\":\"14061313504297296\",\"deviceId\":\"YY2012521173\",\"mileage\":6306,\"fuel\":786,\"speed\":35,\"trackTime\":651,\"startTime\":\"2017-02-22 10:26:11\",\"endTime\":\"2017-02-22 10:37:02\",\"avgFuel\":12.46,\"overspeedNum\":0,\"accNum\":0,\"decNum\":0,\"turnNum\":0,\"brakesNum\":0,\"fuelScore\":0.0,\"overspeedScore\":14.0,\"accScore\":14.0,\"decScore\":14.0,\"turnScore\":14.0,\"brakeScore\":14.0,\"matchScore\":70.0,\"matchDate\":\"2017-02-22 00:00:00\",\"recordTime\":\"2017-03-30 09:58:50\"}]}}";
+        DrivingFingerprintIndexMatchResponse drivingFingerprintIndexMatchResponse = mapper.readValue(json, DrivingFingerprintIndexMatchResponse.class);
+        System.out.println(drivingFingerprintIndexMatchResponse);
+    }
+
+
+
 }
