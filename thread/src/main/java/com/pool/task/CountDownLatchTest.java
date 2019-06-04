2e9d59b3582496e5940d2c789c6bd864f70bd841
xuhongda <xuhongda7@outlook.com>
2019-06-04 15:14:36 +0800

    线程策略

--- thread/src/main/java/com/pool/task/CountDownLatchTest.java
+++ thread/src/main/java/com/pool/task/CountDownLatchTest.java
@@ -1,55 +1,56 @@
 package com.pool.task;
 
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author xuhongda on 2019/3/26
  * com.pool.task
  * java-action
  */
+
+@Slf4j
 public class CountDownLatchTest {
 
+    private static final CountDownLatch latch = new CountDownLatch(2);
 
     public static void main(String[] args) {
 
-        final CountDownLatch latch = new CountDownLatch(2);
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
 
-        new Thread(){
-            @Override
-            public void run() {
-                try {
-                    System.out.println("子线程"+Thread.currentThread().getName()+"正在执行");
-                    Thread.sleep(3000);
-                    System.out.println("子线程"+Thread.currentThread().getName()+"执行完毕");
-                    latch.countDown();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            };
-        }.start();
+        executorService.submit(getTask("A",300L));
 
-        new Thread(){
-            @Override
-            public void run() {
-                try {
-                    System.out.println("子线程"+Thread.currentThread().getName()+"正在执行");
-                    Thread.sleep(3000);
-                    System.out.println("子线程"+Thread.currentThread().getName()+"执行完毕");
-                    latch.countDown();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            };
-        }.start();
+        executorService.submit(getTask("B",100L));
+        long count = latch.getCount();
+        log.info("count = {}",count);
+        executorService.shutdown();
 
         try {
             System.out.println("等待2个子线程执行完毕...");
             latch.await();
+           // latch.await(5,TimeUnit.SECONDS);
             System.out.println("2个子线程已经执行完毕");
             System.out.println("继续执行主线程");
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static Runnable getTask(String str,Long time) {
+        return () -> {
+            try {
+                Thread.sleep(time);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            log.info("线程 = {}",Thread.currentThread().getName());
+            System.out.println(str);
+            latch.countDown();
+        };
     }
 }
--- thread/src/main/java/com/pool/task/CyclicBarrierTest.java
+++ thread/src/main/java/com/pool/task/CyclicBarrierTest.java
@@ -7,6 +7,11 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
+ * <p>
+ *     CyclicBarrier可以使一定数量的线程反复地在栅栏位置处汇集。当线程到达栅栏位置时将调用await方法，
+ *     这个方法将阻塞直到所有线程都到达栅栏位置。如果所有线程都到达栅栏位置，那么栅栏将打开，此时所有的线程都将被释放，
+ *     而栅栏将被重置以便下次使用。
+ * </p>
  * @author xuhongda on 2019/5/16
  * com.pool.task
  * java-action
