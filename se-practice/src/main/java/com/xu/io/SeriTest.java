4000808345896cdb3f370142026f4254e86f1ae7
xuhongda <xuhongda7@outlook.com>
2019-06-19 18:17:08 +0800

    java IO

---
+++ se-practice/src/main/java/com/xu/io/SeriTest.java
@@ -0,0 +1,50 @@
+package com.xu.io;
+
+import org.junit.Test;
+import pojo.Girl;
+
+import java.io.*;
+
+/**
+ * @author xuhongda on 2019/6/19
+ * com.xu.io
+ * java-action
+ */
+public class SeriTest {
+
+
+    @Test
+    public void se001() throws IOException {
+
+        Girl girl = new Girl("xuhongda",18,50.50);
+        //序列化
+        FileOutputStream outputStream = new FileOutputStream("acc.text");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+        objectOutputStream.writeObject(girl);
+    }
+
+    @Test
+    public void se002() throws IOException, ClassNotFoundException {
+
+        Girl girl = new Girl("yan",18,50.50);
+
+
+        File file = new File("acc.text");
+        boolean exists = file.exists();
+        if (exists){
+            //序列化
+           // FileOutputStream outputStream = new FileOutputStream("acc.text");
+            FileInputStream inputStream = new FileInputStream("acc.text");
+
+
+            FileOutputStream objectOutputStream = new FileOutputStream("acc.text");
+            byte[] b = new byte[1024];
+            int len;
+            while ((len = inputStream.read()) != -1){
+                objectOutputStream.write(b,0,len);
+            }
+
+        }
+
+    }
+}
