a04df4322e82bae8161ae9f14cdf9ff7e985a94e
xuhongda <xuhongda7@outlook.com>
2019-07-26 16:49:50 +0800

    注释

--- thread/src/main/java/com/pool/CacheThreadTest.java
+++ thread/src/main/java/com/pool/CacheThreadTest.java
@@ -17,7 +17,7 @@ public class CacheThreadTest {
         // new ThreadPoolExecutor(0, Integer.MAX_VALUE,
         //                                      60L, TimeUnit.SECONDS,
         //                                      new SynchronousQueue<Runnable>());
-        // 原则上可以创建无限大小的线程池，可能导致oom
+        // 原则上可以创建无限大小的线程池，可能导致oom;适用于任务时间比较小的线程
         ExecutorService executorService = Executors.newCachedThreadPool();
 
         executorService.submit(()-> log.info("what is cacheThread?"));
