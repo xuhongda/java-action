d8f00520a02eeb436117265b2d07808c3d22b5ee
xuhongda <xuhongda7@outlook.com>
2019-08-07 15:06:12 +0800

    time

--- se-practice/src/main/java/com/xu/time/TimeTest.java
+++ se-practice/src/main/java/com/xu/time/TimeTest.java
@@ -1,13 +1,23 @@
 package com.xu.time;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * @author xuhongda on 2019/8/7
  * com.xu.time
  * java-action
  */
+@Slf4j
 public class TimeTest {
+
+
+    private SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 
     /**
      * 秒转时分秒
@@ -21,5 +31,14 @@ public class TimeTest {
         s = s%60;
         int M = s;
         System.out.println("时间是："+N+"小时 "+K+"分钟 "+M+"秒");
+    }
+
+
+    @Test
+    public void test002(){
+        Calendar instance = Calendar.getInstance(TimeZone.getDefault());
+        instance.setTime(new Date());
+        instance.add(Calendar.MONTH,2);
+        log.info("time = {}",format.format(instance.getTime()));
     }
 }
