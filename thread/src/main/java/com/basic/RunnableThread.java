6e5d4dd6d3e610a84c009611c701e469fed60332
xuhongda <xuhongda7@outlook.com>
2019-09-29 15:17:30 +0800

    code

--- thread/src/main/java/com/basic/RunnableThread.java
+++ thread/src/main/java/com/basic/RunnableThread.java
@@ -1,14 +1,17 @@
 package com.basic;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author xuhongda on 2018/8/16
  * com.basic
  * javase-practice
  */
+@Slf4j
 public class RunnableThread implements Runnable {
 
     @Override
     public  void run() {
-        System.out.println("is runnable "+Thread.currentThread().getName());
+        log.info("start ...");
     }
 }
