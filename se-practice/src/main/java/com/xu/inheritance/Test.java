969edbbb08b28d84ea680ff2e65b6601c49a11a1
xuhongda <xuhongda7@outlook.com>
2019-10-29 11:36:06 +0800

    log info

--- se-practice/src/main/java/com/xu/inheritance/Test.java
+++ se-practice/src/main/java/com/xu/inheritance/Test.java
@@ -1,15 +1,19 @@
 package com.xu.inheritance;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author xuhongda 2018/6/27
  * com.xu.inheritance
  * javase-practice
  */
+@Slf4j
 public class Test {
     @org.junit.Test
     public void test001(){
         Employee employee = new Manager();
         //会调用 Manager 的方法
         String age = employee.getAge();
+        log.info("age = {}",age);
     }
 }
