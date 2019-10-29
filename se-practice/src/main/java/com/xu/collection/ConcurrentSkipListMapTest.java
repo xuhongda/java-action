1f94a98c54bbc6e240c439bcc12117bd7efdc0c5
xuhongda <xuhongda7@outlook.com>
2019-10-29 11:38:02 +0800

    skipMap

--- se-practice/src/main/java/com/xu/collection/ConcurrentSkipListMapTest.java
+++ se-practice/src/main/java/com/xu/collection/ConcurrentSkipListMapTest.java
@@ -2,9 +2,8 @@ package com.xu.collection;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
-import pojo.Girl;
 
-import java.util.Map;
+import java.util.NavigableSet;
 import java.util.concurrent.ConcurrentNavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 
@@ -16,54 +15,45 @@ import java.util.concurrent.ConcurrentSkipListMap;
 @Slf4j
 public class ConcurrentSkipListMapTest {
 
-    private ConcurrentSkipListMap<Object, Object> map;
+    private ConcurrentSkipListMap<String, String> map = new ConcurrentSkipListMap<>();
 
     {
         log.info("init...");
-        map = new ConcurrentSkipListMap<>();
-        map.put("1", 1);
-        map.put("2", 2);
-    }
-
-    @Test
-    public void test001() {
-        map.put("1", new Girl("yan", 18, 50));
-        map.put("2", 2);
-        map.put("3", 2);
-        map.put("4", new Girl("yan", 18, 50));
-        log.info("map = {}", map);
+        map.put("1", "1");
+        map.put("2", "2");
+        map.put("3", "3");
+        map.put("4", "4");
+        map.put("5", "5");
+        map.put("6", "6");
     }
 
 
-    @Test
-    public void test002() {
-        map.put("1", 1);
-        map.put("2", 2);
-        ConcurrentNavigableMap<Object, Object> objectObjectConcurrentNavigableMap = map.descendingMap();
-        log.info("map = {}", objectObjectConcurrentNavigableMap);
-    }
-
 
     @Test
-    public void test003() {
-        map.put("xx", "yy");
-        Object o = map.ceilingKey("3");
-        log.info("object  = {}", o);
-    }
+    public void test005() {
 
-    /**
-     * pollFirstEntry
-     */
-    @Test
-    public void test004() {
+        log.info("map = {}",map);
+        ConcurrentNavigableMap<String, String> headMap = map.headMap("4");
+        System.out.println(headMap);
+        NavigableSet<String> set = headMap.keySet();
+        /*for (String s : set){
+            map.remove(s);
+        }*/
+        headMap.clear();
         System.out.println(map);
-        log.info("size = {}",map.size());
-        Map.Entry<Object, Object> objectObjectEntry = map.pollFirstEntry();
-        //Map.Entry<Object, Object> objectObjectEntry1 = map.pollLastEntry();
-        Object value = objectObjectEntry.getValue();
-        log.info("object  = {}", value);
-        log.info("size = {}",map.size());
+
+    }
+
+
+    @Test
+    public void test006() {
+
+        log.info("map = {}",map);
+        ConcurrentNavigableMap<String, String> headMap = map.headMap("4");
+        headMap.clear();
+        map.remove("4");
         System.out.println(map);
+
     }
 
 }
