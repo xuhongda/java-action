55bee391e7689d245fd0ab39a9c06258960eff7f
xuhongda <xuhongda7@outlook.com>
2020-10-13 10:46:23 +0800

    add something

--- component/src/main/test/java/com/xu/test/GdpTest.java
+++ component/src/main/test/java/com/xu/test/GdpTest.java
@@ -38,14 +38,14 @@ public class GdpTest {
                 v = v.multiply(vInc);
                 usa = usa.multiply(usaInc);
                 china = china.multiply(chinaInc);
-            } else if ( i < 15) {
+            } else if (i < 15) {
                 usaInc = new BigDecimal("1.015");
                 chinaInc = new BigDecimal("1.03");
                 vInc = new BigDecimal("1.045");
                 v = v.multiply(vInc);
                 usa = usa.multiply(usaInc);
                 china = china.multiply(chinaInc);
-            } else if (i< 30){
+            } else if (i < 30) {
                 usaInc = new BigDecimal("1.005");
                 chinaInc = new BigDecimal("1.015");
                 vInc = new BigDecimal("1.02");
@@ -58,25 +58,25 @@ public class GdpTest {
 
             if (Math.abs(v.subtract(usa).doubleValue()) < 1000) {
                 log.info("usa == v;year = {};value = {}", i, v.doubleValue());
-                  flag = true;
+                flag = true;
             }
 
             if (Math.abs(v.subtract(china).doubleValue()) < 1000) {
                 log.info("china == v;year = {};value = {}", i, v.doubleValue());
-                 flag = true;
+                flag = true;
             }
 
             if (Math.abs(china.subtract(usa).doubleValue()) < 1000) {
                 log.info("china == usa;year = {};value = {}", i, china.doubleValue());
-                 flag =true;
+                flag = true;
             }
 
-            if (i>50){
+            if (i > 50) {
                 log.info("The three country's GDP will not equal in recent 100 years");
                 flag = true;
             }
 
-            if (flag){
+            if (flag) {
                 break;
             }
         }
@@ -88,4 +88,41 @@ public class GdpTest {
         double abs = Math.abs(-0.06);
         System.out.println(abs);
     }
+
+    /**
+     * 收益计算
+     */
+    @Test
+    public void test003() {
+
+        BigDecimal initial = new BigDecimal("1");
+
+        BigDecimal factor = new BigDecimal("1.3");
+
+        int years = 0;
+        boolean f = true;
+        for (; ; ) {
+            years++;
+            //倍数
+            initial = initial.multiply(factor);
+            BigDecimal initialM = new BigDecimal("300000");
+            //金额
+            initialM = initialM.multiply(initial);
+
+            log.info("year ={} ,now value = {},now money = {}", years, initial.doubleValue(), initialM);
+
+            if (initial.subtract(new BigDecimal("1")).doubleValue() >= 1) {
+                if (f) {
+                    log.info("After {} years the value have double ", years);
+                }
+                f = false;
+            }
+
+            if (years == 10) {
+                break;
+            }
+        }
+    }
+
+
 }
