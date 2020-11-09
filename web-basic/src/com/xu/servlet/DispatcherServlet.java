3b4aebe23da46485cde4fddc4d5967f0d59b70f4
xuhongda <xuhongda7@outlook.com>
2020-11-09 17:37:28 +0800

    some changes

--- web-basic/src/com/xu/servlet/DispatcherServlet.java
+++ web-basic/src/com/xu/servlet/DispatcherServlet.java
@@ -1,8 +1,6 @@
 package com.xu.servlet;
 
 import lombok.extern.slf4j.Slf4j;
-
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
