21e5b9fe4175403e9da578cbea21001f1a5c47d0
xuhongda <xuhongda7@outlook.com>
2020-11-27 14:31:52 +0800

    exception

--- se-practice/src/main/java/com/xu/exception/test/ExceptionTest.java
+++ se-practice/src/main/java/com/xu/exception/test/ExceptionTest.java
@@ -1,6 +1,5 @@
 package com.xu.exception.test;
 
-import com.xu.collection.MyArrayList;
 import com.xu.exception.MyException;
 import com.xu.exception.MyRunException;
 import lombok.extern.slf4j.Slf4j;
@@ -19,45 +18,43 @@ public class ExceptionTest {
 
         try {
             throw new MyRunException("this is my runtime exception");
-        }catch (MyRunException e){
+        } catch (MyRunException e) {
             log.info(e.toString());
         }
 
     }
 
 
-
     @Test
-    public void test002(){
+    public void test002() {
         try {
             throw new MyException("this is my  exception");
-        }catch (MyException e){
+        } catch (MyException e) {
             log.info(e.toString());
         }
     }
 
 
     @Test
-    public void test003(){
+    public void test003() {
 
         ex1();
-
         try {
             ex2();
         } catch (MyException e) {
-            e.printStackTrace();
+            log.info("ex2= {}", e.toString());
+        } finally {
+            log.info("...");
         }
     }
 
 
-    private String ex1() throws MyRunException{
-
-        return "this is my runtime exception";
+    private void ex1() throws MyRunException {
+        throw new MyRunException("this is my runtime exception");
     }
 
 
-    private String ex2() throws MyException{
-
-        return "this is my  exception";
+    private void ex2() throws MyException {
+        throw new MyException("this is my exception");
     }
 }
