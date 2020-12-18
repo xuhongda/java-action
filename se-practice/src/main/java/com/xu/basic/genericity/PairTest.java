885af178351f091981b5d5647bc5eebc7d944e80
xuhongda <xuhongda7@outlook.com>
2020-12-18 17:23:47 +0800

    Serializable

--- se-practice/src/main/java/com/xu/basic/genericity/PairTest.java
+++ se-practice/src/main/java/com/xu/basic/genericity/PairTest.java
@@ -1,8 +1,11 @@
 package com.xu.basic.genericity;
 
+import com.xu.algorithm.ArrayAlgorithm;
 import org.junit.Test;
 
 import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * 泛型类测试
@@ -15,19 +18,30 @@ public class PairTest {
     @Test
     public void test(){
         String[] words = {"yan", "xu", "jack", "cr7"};
-        Pair<String> pair = ArrayAlg.minmax(words);
+        Pair<String> pair = ArrayAlgorithm.minmax(words);
         System.out.println(pair.getFirst());
         System.out.println(pair.getSecond());
     }
     @Test
     public void test2(){
-        String middle = ArrayAlg.getMiddle("xx", "dd","xx");
+        String middle = ArrayAlgorithm.getMiddle("xx", "dd","xx");
         System.out.println(middle);
     }
     @Test
     public void test3(){
         //参数是多种类型，包装和String 都实现了 Serializable 接口
-        Serializable xx = ArrayAlg.getMiddle(1.22, 44,0.66,"zzz");
+        Serializable xx = ArrayAlgorithm.getMiddle(1.22, 44,0.66,"zzz");
         System.out.println(xx);
+    }
+
+    @Test
+    public void test4(){
+        Calendar instance1 = Calendar.getInstance();
+        Calendar instance2 = Calendar.getInstance();
+        instance1.setTime(new Date(99999L));
+        instance2.setTime(new Date(10000L));
+        Calendar [] array = {instance1,instance2};
+        Calendar min = ArrayAlgorithm.min(array);
+        System.out.println(min.getTime());
     }
 }
--- se-practice/src/main/java/com/xu/io/SerializeTest.java
+++ se-practice/src/main/java/com/xu/io/SerializeTest.java
@@ -2,28 +2,32 @@ package com.xu.io;
 
 import org.junit.Test;
 import pojo.Girl;
-
 import java.io.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 /**
  * @author xuhongda on 2019/6/19
  * com.xu.io
  * java-action
  */
-public class SerialTest {
+public class SerializeTest {
 
 
     @Test
     public void se001() throws IOException {
-
-        Girl girl = new Girl("xuhongda",18,50.50);
+        Girl girl = new Girl("yan", 18, 50.50);
         //序列化
         FileOutputStream outputStream = new FileOutputStream("acc.text");
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
         objectOutputStream.writeObject(girl);
+    }
+
+
+    @Test
+    public void se00x() throws IOException, ClassNotFoundException {
+        //反序列化
+        ObjectInputStream outputStream = new ObjectInputStream(new FileInputStream("acc.text"));
+        Girl girl = (Girl) outputStream.readObject();
+        System.out.println(girl);
     }
 
     @Test
@@ -31,9 +35,9 @@ public class SerialTest {
 
         File file = new File("acc.text");
         boolean exists = file.exists();
-        if (exists){
+        if (exists) {
             //序列化
-           // FileOutputStream outputStream = new FileOutputStream("acc.text");
+            // FileOutputStream outputStream = new FileOutputStream("acc.text");
             FileInputStream inputStream = new FileInputStream("acc.text");
             FileOutputStream objectOutputStream = new FileOutputStream("bcc.text");
             ObjectOutputStream outputStream = new ObjectOutputStream(objectOutputStream);
@@ -41,26 +45,12 @@ public class SerialTest {
             System.out.println(read);
             byte[] b = new byte[1024];
             int len;
-            while ((len = inputStream.read()) != -1){
-                outputStream.write(b,0,len);
-               // objectOutputStream.write(b,0,len);
+            while ((len = inputStream.read()) != -1) {
+                outputStream.write(b, 0, len);
+                // objectOutputStream.write(b,0,len);
             }
             outputStream.flush();
         }
 
-    }
-
-
-    @Test
-    public void test019() throws ParseException {
-
-
-        String t = "2019-10-17";
-        SimpleDateFormat format2 = new SimpleDateFormat("yyyy-MM-dd");
-        Date parse = format2.parse(t);
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String format1 = format.format(parse);
-
-        System.out.println(format1);
     }
 }
